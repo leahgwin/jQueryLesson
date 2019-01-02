ee53174a101d434683f309d593b61bc55bf89814
leahgwin <leahgwin@gmail.com>
2019-01-02 11:00:38 -0600

    cleared answers to get ready for lesson

--- script.js
+++ script.js
@@ -9,34 +9,25 @@
 // // First, let's go through the basic process of creating and appending an element with vanilla JavaScript and jquery
 
 // create a new div
-const vanilla = document.createElement("div");
-const $jMagic = $("<div>");
 
 // Give your div some text
-vanilla.textContent = "This is vanilla JS!";
-$jMagic.text("HELLO IT IS JQUERY");
 
 // Give it a class of "box"
-vanilla.classList.add("box");
-$jMagic.addClass("box");
 
 // give vanilla an id of "box-a"
-vanilla.id = "box-a";
+
 // give jquery an id of "box-b"
-$jMagic.attr("id", "box-b");
 
 // append it to the DOM
-const containerVanilla = document.querySelector("#vanilla-container");
-containerVanilla.appendChild(vanilla);
 
 // $("#jquery-container").append($jMagic);
-$jMagic.appendTo($("#jquery-container"));
 
 // There are lot of ways to append things with jQuery! I personally like .appendTo() and .prependTo() a lot because they're nice when you want to chain jQuery methods.
 
 // Now let's look at it all in one line
-$("<div>")
-  .text("HELLO ITS JQUERY ALL IN ONE LINE!!!!")
-  .addClass("box")
-  .attr("id", "box-b")
-  .appendTo($("#jquery-container"));
+
+// $("<div>")
+//   .text("HELLO ITS JQUERY ALL IN ONE LINE!!!!")
+//   .addClass("box")
+//   .attr("id", "box-b")
+//   .appendTo($("#jquery-container"));
